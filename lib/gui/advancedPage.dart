ae1e0b4395b6a4c2af1779e3fb678c4efc522cec
Peppe289 <gsperanza204@protonmail.com>
2023-12-28 14:13:40 +0100

    advanced page: populate with info
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- lib/gui/advancedPage.dart
+++ lib/gui/advancedPage.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import '../utils/RamManage.dart';
 import 'package:just_toast/just_toast.dart';
+import 'package:system_info2/system_info2.dart';
 
 // ignore: camel_case_types
 class buildSecondPage extends StatelessWidget {
@@ -13,9 +14,16 @@ class buildSecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Advanced Page for manage settings',
-            style: TextStyle(color: Colors.white)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            buildRow('Kernel Version:', SysInfo.kernelVersion),
+            buildRow('Architecture:', SysInfo.kernelArchitecture.toString()),
+            buildRow('Physical Memory Size:', '${(SysInfo.getTotalPhysicalMemory() / 1000000).truncate()} MB'),
+            buildRow('Virtual Memory Size:', '${(SysInfo.getTotalVirtualMemory() / 1000000).truncate()} MB'),
+          ],
+        ),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -26,6 +34,25 @@ class buildSecondPage extends StatelessWidget {
           },
           child: const Text('Clear RAM'),
         ),
+      ),
+    );
+  }
+
+  Widget buildRow(String leftText, String rightParam) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(50.0, 20.0, 50.0, 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            leftText,
+            style: const TextStyle(color: Colors.white),
+          ),
+          Text(
+            rightParam,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ],
       ),
     );
   }
