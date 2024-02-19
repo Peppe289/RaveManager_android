420c4c9c895df80e9fc1cb079036baf00e2651a3
Peppe289 <gsperanza204@protonmail.com>
2024-02-19 16:07:26 +0100

    rework process list
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- lib/utils/ProcessList.dart
+++ lib/utils/ProcessList.dart
@@ -3,7 +3,10 @@
 //import 'dart:io';
 import 'dart:convert';
 import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+import 'dart:math';
 import 'package:root/root.dart';
+import 'dart:convert';
 
 class ProcessList {
   late String name;
@@ -30,39 +33,44 @@ class ProcessList {
   }
 }
 
+final class TaskSimplyStruct extends Struct {
+  @Int32()
+  external int pid;
+
+  @Array(16)
+  external Array<Uint8> comm;
+}
+
 class LoadProcessList {
   late List<int> pid;
   late List<ProcessList> full = [];
 
   void updateList() async {
-    String? res;
-    try {
-      res = await Root.exec(cmd: "ps -e -o pid,cmd");
-    } catch (e) {
-      full.add(ProcessList('Err', 0, 0, 0));
-      return;
+    full.clear();
+
+    final lib = DynamicLibrary.open("librave.so");
+    final sizePtr = calloc<Int32>();
+    final taskList = lib.lookupFunction<
+        Pointer<TaskSimplyStruct> Function(Pointer<Int32>),
+        Pointer<TaskSimplyStruct> Function(Pointer<Int32>)>("task_list");
+
+    Pointer<TaskSimplyStruct> resultPtr = taskList(sizePtr);
+ 
+    for (int i = 0; i < sizePtr[0]; ++i) {
+      int virt = 0;
+      int res = 0;
+
+      Array<Uint8> tmp = resultPtr[i].comm;
+      List<int> stInt32 = [];
+      for (int k = 0; k < 16; ++k) {
+        stInt32.add(tmp[k]);
+      }
+
+      full.add(ProcessList(utf8.decode(stInt32), resultPtr[i].pid, virt, res));
     }
 
-      List<String> output =
-          LineSplitter.split(res as String).toList();
-      full.clear();
-      for (var line in output) {
-        List<String> split = line.trim().toString().split(' ');
-        // is first line. we don't need this
-        if (split[0].compareTo("PID") == 0) {
-          continue;
-        }
-        int pid = int.parse(split[0]);
-        int virt = 0;
-        int res = 0;
-
-        final lib = DynamicLibrary.open("librave.so");
-        final uselib = lib.lookupFunction<Int32 Function(), int Function()>("someData");
-
-        full.add(ProcessList(split[1], uselib(), virt, res));
-      }
-    
-
+    calloc.free(sizePtr);
+    calloc.free(resultPtr);
     return;
   }
 
