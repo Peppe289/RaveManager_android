a55f452ad8ea586672612bbbad0affde14c7d427
Peppe289 <gsperanza204@protonmail.com>
2024-02-19 16:54:32 +0100

    ProcessList: improve code
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- lib/utils/ProcessList.dart
+++ lib/utils/ProcessList.dart
@@ -1,12 +1,66 @@
 // ignore_for_file: file_names
-
-//import 'dart:io';
 import 'dart:convert';
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
-import 'dart:math';
-import 'package:root/root.dart';
-import 'dart:convert';
+
+final class TaskSimplyStruct extends Struct {
+  @Int32()
+  external int pid;
+
+  @Array(16)
+  external Array<Uint8> comm;
+}
+
+class LoadProcessList {
+  late List<int> pid;
+  // List of all process (take from PCB) 
+  late List<ProcessList> full = [];
+
+  // ignore: non_constant_identifier_names
+  List<int> convert_Uint8_to_int(Array<Uint8> data) {
+    Array<Uint8> tmp = data;
+    List<int> stInt32 = [];
+
+    // 16 is for comm[16] in the kernel structure about PCB
+    for (int k = 0; k < 16; ++k) {
+      stInt32.add(tmp[k]);
+    }
+
+    return stInt32;
+  }
+
+  void updateList() async {
+    full.clear();
+
+    final lib = DynamicLibrary.open("librave.so");
+    final sizePtr = calloc<Int32>();
+    final taskList = lib.lookupFunction<
+        Pointer<TaskSimplyStruct> Function(Pointer<Int32>),
+        Pointer<TaskSimplyStruct> Function(Pointer<Int32>)>("task_list");
+
+    Pointer<TaskSimplyStruct> resultPtr = taskList(sizePtr);
+
+    for (int i = 0; i < sizePtr[0]; ++i) {
+      int virt = 0;
+      int res = 0;
+
+      full.add(ProcessList(utf8.decode(convert_Uint8_to_int(resultPtr[i].comm)),
+          resultPtr[i].pid, virt, res));
+    }
+
+    calloc.free(sizePtr);
+    calloc.free(resultPtr);
+    return;
+  }
+
+  List<ProcessList> getList() {
+    return full;
+  }
+
+  int getLength() {
+    return full.length;
+  }
+}
 
 class ProcessList {
   late String name;
@@ -30,55 +84,5 @@ class ProcessList {
 
   int getRes() {
     return res;
-  }
-}
-
-final class TaskSimplyStruct extends Struct {
-  @Int32()
-  external int pid;
-
-  @Array(16)
-  external Array<Uint8> comm;
-}
-
-class LoadProcessList {
-  late List<int> pid;
-  late List<ProcessList> full = [];
-
-  void updateList() async {
-    full.clear();
-
-    final lib = DynamicLibrary.open("librave.so");
-    final sizePtr = calloc<Int32>();
-    final taskList = lib.lookupFunction<
-        Pointer<TaskSimplyStruct> Function(Pointer<Int32>),
-        Pointer<TaskSimplyStruct> Function(Pointer<Int32>)>("task_list");
-
-    Pointer<TaskSimplyStruct> resultPtr = taskList(sizePtr);
- 
-    for (int i = 0; i < sizePtr[0]; ++i) {
-      int virt = 0;
-      int res = 0;
-
-      Array<Uint8> tmp = resultPtr[i].comm;
-      List<int> stInt32 = [];
-      for (int k = 0; k < 16; ++k) {
-        stInt32.add(tmp[k]);
-      }
-
-      full.add(ProcessList(utf8.decode(stInt32), resultPtr[i].pid, virt, res));
-    }
-
-    calloc.free(sizePtr);
-    calloc.free(resultPtr);
-    return;
-  }
-
-  List<ProcessList> getList() {
-    return full;
-  }
-
-  int getLength() {
-    return full.length;
   }
 }
